b54f4edd23d466fb244eab660254b1ef40114e4b
ArchGuy <52102426+ArchGuyWu@users.noreply.github.com>
2025-06-07 22:22:33 +0800

    Update start-dinit.sh

--- packages/dinit/start-dinit.sh
+++ packages/dinit/start-dinit.sh
@@ -1,2 +1,3 @@
-export DINIT_SOCKET_PATH="$PREFIX/var/run/dinitctl"
-start-stop-daemon -S -x "$PREFIX/bin/dinit" -b -- -d "$PREFIX/etc/dinit.d" -p "$PREFIX/var/run/dinitctl"
+mkdir -p "$PREFIX/var/run/user"
+export DINIT_SOCKET_PATH="$PREFIX/var/run/user/dinitctl"
+start-stop-daemon -S -x "$PREFIX/bin/dinit" -b -- -d "$PREFIX/etc/dinit.d/user" -p "$PREFIX/var/run/user/dinitctl"
